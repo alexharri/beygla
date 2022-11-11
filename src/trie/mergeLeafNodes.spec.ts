54b2e77efa76a8299921c71526f5f00fcf596996
alexharri <alexharri2919@gmail.com>
2022-11-11 10:27:17 +0000

    add test for letter order insensitiveness

--- src/trie/mergeLeafNodes.spec.ts
+++ src/trie/mergeLeafNodes.spec.ts
@@ -14,4 +14,18 @@ describe("mergeLeafNodes", () => {
     expect(trie.leafAtKeyExists("(l|g)ja")).toEqual(true);
     expect(trie.leafAtKeyExists("rja")).toEqual(true);
   });
+
+  it("is not sensitive to letter order when accessing merged leaf nodes", () => {
+    const A = "a";
+    const B = "b";
+    const trie = new Trie()
+      .insert("Silja", A)
+      .insert("Bylgja", A)
+      .insert("Marja", B);
+
+    trie.simplify();
+
+    expect(trie.leafAtKeyExists("(l|g)ja")).toEqual(true);
+    expect(trie.leafAtKeyExists("(g|l)ja")).toEqual(true);
+  });
 });
