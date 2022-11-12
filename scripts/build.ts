2e3502eb06398010f601ea93a30e005392c77456
alexharri <alexharri2919@gmail.com>
2022-11-12 15:40:20 +0000

    update build script to inject the serialized trie into the .js file

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { execSync } from "child_process";
 
 const DIST_DIR = path.resolve(__dirname, "../dist/");
+const OUT_DIR = path.resolve(__dirname, "../out/");
 
 // Remove old build
 execSync(`rm -rf dist/`);
@@ -16,28 +17,51 @@ execSync(`cp README.md package.json dist/`);
 // Remove 'dist/common/' and 'dist/read/'
 execSync(`rm -rf dist/common/ dist/read/`);
 
-// There should be no more directories
-for (const fileName of fs.readdirSync(DIST_DIR)) {
-  const filePath = path.resolve(DIST_DIR, fileName);
-  if (fs.lstatSync(filePath).isDirectory()) {
-    throw new Error(`Unexpected directory '${filePath}'`);
+const emittedJsFileNames = ["beygla.js", "beygla.esm.js"];
+
+const filesThatShouldExist = new Set([
+  "package.json",
+  "README.md",
+  "beygla.d.ts",
+  ...emittedJsFileNames,
+]);
+
+const fileNames = fs.readdirSync(DIST_DIR);
+
+if (fileNames.length !== filesThatShouldExist.size) {
+  throw new Error(
+    `Expected ${filesThatShouldExist.size} files, got '${fileNames.length}'`
+  );
+}
+
+for (const fileName of fileNames) {
+  if (!filesThatShouldExist.has(fileName)) {
+    throw new Error(`Unexpected file/directory '${fileName}'`);
   }
 }
 
-function assertFileExists(filePath: string) {
-  if (fs.existsSync(filePath)) return;
-  throw new Error(`Expected '${filePath}' to exist.`);
+const serializedTrie = fs.readFileSync(
+  path.resolve(OUT_DIR, "./trie-ser.txt"),
+  "utf-8"
+);
+
+if (serializedTrie.length < 1000) {
+  throw new Error(
+    `Serialized trie is unexpectedly short (${serializedTrie.length} characters).`
+  );
 }
 
-const filesThatShouldExist = [
-  "package.json",
-  "README.md",
-  "beygla.js",
-  "beygla.esm.js",
-  "beygla.d.ts",
-];
-
-for (const fileName of filesThatShouldExist) {
+for (const fileName of emittedJsFileNames) {
   const filePath = path.resolve(DIST_DIR, fileName);
-  assertFileExists(filePath);
+  let fileContent = fs.readFileSync(filePath, "utf-8");
+  if (!fileContent.includes(`serializedInput = "@@input@@"`)) {
+    throw new Error(
+      `Expected '${fileName}' to include 'serializedInput = "@@input@@"'`
+    );
+  }
+  fileContent = fileContent.replace(
+    `serializedInput = "@@input@@"`,
+    `serializedInput = "${serializedTrie}"`
+  );
+  fs.writeFileSync(filePath, fileContent, "utf-8");
 }
