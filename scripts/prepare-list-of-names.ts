e208a33148cd81631e3db779526d1258e51178a8
alexharri <alexharri2919@gmail.com>
2022-11-13 08:43:07 +0000

    improve and clarify prepare-list-of-names script

--- scripts/prepare-list-of-names.ts
+++ scripts/prepare-list-of-names.ts
@@ -11,10 +11,24 @@ function main() {
   const names: string[] = [];
 
   for (const line of lines) {
-    let [, name, decision] = line.split(",");
+    let [id, name, decision] = line.split(",");
 
-    // I have seen one example of this
-    if (decision === "NULL") continue;
+    // Skip malformed entry. I have filed a bug in the original data source:
+    //
+    //    https://github.com/island-is/island.is/issues/9010
+    //
+    if (id === "4638") continue;
+
+    // The following names contain a decision of 'NULL'.
+    //
+    // All of these names are not present in 'words.csv' so we treating
+    // them as if they were rejected.
+    switch (id) {
+      case "4692": // Emelíta
+      case "5113": // Ivý
+        decision = `"Haf"`;
+        break;
+    }
 
     // Remove surrounding "" from name and decision
     name = name.substr(1, name.length - 2);
@@ -22,14 +36,13 @@ function main() {
 
     switch (decision) {
       case "Sam": // Accepted
-      case "": // Older name, accepted by-default
         names.push(name);
         break;
       case "Haf": // Rejected
       case "Óaf": // Not processed
         break;
       default:
-        throw new Error(`Unexpected decision '${decision}'`);
+        throw new Error(`Unexpected decision '${decision}' on line '${line}'`);
     }
   }
 
