f67cc152b5a9e69124923dce6a0c33db6a4ddbb5
alexharri <alexharri2919@gmail.com>
2024-05-18 11:47:25 +0000

    update comments

--- scripts/filter-addresses.ts
+++ scripts/filter-addresses.ts
@@ -1,6 +1,6 @@
 // This script filters the very, very large 'word-cases.csv' file into a
-// 'name-cases.csv' file that only contains the lines in 'word-cases.csv' that
-// correspond to names contained in from 'icelandic-names.json'.
+// 'address-cases.csv' file that only contains the lines in 'word-cases.csv' that
+// correspond to addresses contained in 'icelandic-addresses.json'.
 
 import fs from "fs/promises";
 import fsSync from "fs";
@@ -10,17 +10,17 @@ import { logWriteAndSize } from "../lib/preprocess/utils/gzip";
 import { getRawName } from "../lib/preprocess/format/name";
 import { isFirstVariationOfCase } from "../lib/preprocess/format/case";
 
-const nameCasesCsvFilePath = path.resolve(
+const addressCasesCsvFilePath = path.resolve(
   __dirname,
   "../out/address-cases.csv"
 );
 const wordCasesCsvFilePath = path.resolve(__dirname, "../data/word-cases.csv");
 
 async function main() {
-  await fs.writeFile(nameCasesCsvFilePath, "", "utf-8");
+  await fs.writeFile(addressCasesCsvFilePath, "", "utf-8");
   const inputFile = await fs.open(wordCasesCsvFilePath);
 
-  const outStream = fsSync.createWriteStream(nameCasesCsvFilePath, "utf-8");
+  const outStream = fsSync.createWriteStream(addressCasesCsvFilePath, "utf-8");
 
   let nInputLines = 0;
   let nOutputLines = 0;
@@ -45,7 +45,7 @@ async function main() {
 
   console.log(`Filtered ${nInputLines} entries into ${nOutputLines} entries.`);
 
-  logWriteAndSize(nameCasesCsvFilePath);
+  logWriteAndSize(addressCasesCsvFilePath);
 }
 
 main();
